08a9e22cef3f1fb944f1a89dfe60baa6b2b1b79a
Sarthak <androiddeveloper251@gmail.com>
2023-09-16 23:31:38 -0400

    moved some things around

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,61 +1,63 @@
-import "./globals.css";
-import ProfileCard from "../components/Sidebar/ProfileCard";
-import Link from "next/link";
-import Providers from "./Providers";
-import { SearchBar, UserAvatar } from "@/utilities/UtilityComponents";
-import Sidebar from "@/components/Sidebar/Sidebar";
+import './globals.css'
+import ProfileCard from '../components/Sidebar/ProfileCard'
+import Link from 'next/link'
+import Providers from './Providers'
+import { SearchBar, UserAvatar } from '@/utilities/UtilityComponents'
+import Sidebar from '@/components/Sidebar/Sidebar'
+import { register } from 'swiper/element/bundle'
 
-import { Ubuntu } from "next/font/google";
-import DrawerSidebar from "@/components/DrawerSidebar";
+import { Ubuntu } from 'next/font/google'
+import DrawerSidebar from '@/components/DrawerSidebar'
 
 export const metadata = {
-  title: "Venato",
-  description: "Your game manager",
-};
+	title: 'Venato',
+	description: 'Your game manager',
+}
 
 const ubuntu = Ubuntu({
-  weight: "400",
-  style: "normal",
-  subsets: ["latin"],
-});
+	weight: '400',
+	style: 'normal',
+	subsets: ['latin'],
+})
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en" className={ubuntu.className}>
-      <body className="flex h-screen flex-col overflow-hidden">
-        <Providers>
-          <div className="flex h-full flex-col">
-            <header className="flex items-center justify-between gap-3 p-3 px-6 shadow-md shadow-zinc-800">
-              <div className="flex items-center gap-4">
-                <DrawerSidebar />
-                <Link
-                  href="/"
-                  className="hidden text-4xl font-bold text-yellow-500 min-[500px]:flex "
-                >
-                  Venato
-                </Link>
-              </div>
-              <SearchBar />
+	register()
+	return (
+		<html lang="en" className={ubuntu.className}>
+			<body className="flex h-screen flex-col overflow-hidden">
+				<Providers>
+					<div className="flex h-full flex-col">
+						<header className="flex items-center justify-between gap-3 p-3 px-6 shadow-md shadow-zinc-800">
+							<div className="flex items-center gap-4">
+								<DrawerSidebar />
+								<Link
+									href="/"
+									className="hidden text-4xl font-bold text-yellow-500 min-[500px]:flex "
+								>
+									Venato
+								</Link>
+							</div>
+							<SearchBar />
 
-              <UserAvatar text="SM" />
-            </header>
-            <div className="flex h-[calc(100vh-110px)]">
-              <nav className="my-6 ml-6 hidden h-full w-60 min-w-[300px] flex-col justify-between rounded-2xl border-2 border-zinc-700 bg-zinc-900 p-4 lg:flex">
-                <div className=" flex flex-col ">
-                  <Sidebar className="" />
-                </div>
-              </nav>
-              <main className="my-6 h-full w-full overflow-y-auto text-white">
-                <div className=" h-full">{children}</div>
-              </main>
-            </div>
-          </div>
-        </Providers>
-      </body>
-    </html>
-  );
+							<UserAvatar text="SM" />
+						</header>
+						<div className="flex h-[calc(100vh-110px)]">
+							<nav className="my-6 ml-6 hidden h-full w-60 min-w-[300px] flex-col justify-between rounded-2xl border-2 border-zinc-700 bg-zinc-900 p-4 lg:flex">
+								<div className=" flex flex-col ">
+									<Sidebar className="" />
+								</div>
+							</nav>
+							<main className="my-6 h-full w-full overflow-y-auto text-white">
+								<div className=" h-full">{children}</div>
+							</main>
+						</div>
+					</div>
+				</Providers>
+			</body>
+		</html>
+	)
 }
--- src/components/BigGameCard/BigGameCard.jsx
+++ src/components/BigGameCard/BigGameCard.jsx
@@ -1,60 +1,60 @@
-import style from "./CenterStage.module.scss";
-import { Button } from "@/components/ui/button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import StarRating from "./StarRating";
-import { MetacriticSquare } from "@/utilities/UtilityComponents";
+import style from './CenterStage.module.scss'
+import { Button } from '@/components/ui/button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import StarRating from '@/utilities/StarRating'
+import { MetacriticSquare } from '@/utilities/UtilityComponents'
 
 const GameCardCenterStage = ({ game }) => {
-  let { id, slug, name, bgImage, rating, metacritic, released } = game;
+	let { id, slug, name, bgImage, rating, metacritic, released } = game
 
-  const releasedDate = new Date(released).toLocaleString("en-us", {
-    month: "long",
-    year: "numeric",
-  });
+	const releasedDate = new Date(released).toLocaleString('en-us', {
+		month: 'long',
+		year: 'numeric',
+	})
 
-  return (
-    <div
-      className={`flex h-[40vh] w-full overflow-visible p-1 pt-[2px] sm:h-[77.5vh] sm:p-2`}
-    >
-      <div
-        style={{
-          backgroundImage: `url(${bgImage})`,
-          borderRadius: "16px",
-        }}
-        className={` game-card-content flex h-full w-full text-white ${style.bgimage} overflow-hidden outline outline-2 outline-amber-500/70`}
-      >
-        <div className="z-10 w-full">
-          <div className="left flex h-full max-w-[1000px] flex-col items-start justify-end gap-4  ">
-            <div className="flex flex-col items-start gap-2 rounded-bl-2xl rounded-tr-2xl bg-black bg-opacity-50 bg-clip-padding p-2 pb-0 pl-3 pr-4 shadow-lg backdrop-blur-md backdrop-filter sm:p-4 sm:px-6">
-              <p className="text-lg font-bold sm:text-5xl">{name}</p>
+	return (
+		<div
+			className={`flex h-[40vh] w-full overflow-visible p-1 pt-[2px] sm:h-[77.5vh] sm:p-2`}
+		>
+			<div
+				style={{
+					backgroundImage: `url(${bgImage})`,
+					borderRadius: '16px',
+				}}
+				className={` game-card-content flex h-full w-full text-white ${style.bgimage} overflow-hidden outline outline-2 outline-amber-500/70`}
+			>
+				<div className="z-10 w-full">
+					<div className="left flex h-full max-w-[1000px] flex-col items-start justify-end gap-4  ">
+						<div className="flex flex-col items-start gap-2 rounded-bl-2xl rounded-tr-2xl bg-black bg-opacity-50 bg-clip-padding p-2 pb-0 pl-3 pr-4 shadow-lg backdrop-blur-md backdrop-filter sm:p-4 sm:px-6">
+							<p className="text-lg font-bold sm:text-5xl">{name}</p>
 
-              <p className="text-xs text-gray-300 sm:mb-4 sm:text-sm">
-                {releasedDate}
-              </p>
-              <div className="flex items-center justify-center gap-4 sm:pb-2">
-                <Button
-                  className="hidden sm:block"
-                  ghost
-                  size="lg"
-                  color="warning"
-                >
-                  Check it out
-                </Button>
-                <StarRating className="hidden sm:block" rating={rating} />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="right z-10 flex h-full items-start justify-end">
-          <MetacriticSquare
-            className="p-3 text-sm sm:p-4 sm:text-xl"
-            score={parseInt(metacritic)}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
+							<p className="text-xs text-gray-300 sm:mb-4 sm:text-sm">
+								{releasedDate}
+							</p>
+							<div className="flex items-center justify-center gap-4 sm:pb-2">
+								<Button
+									className="hidden sm:block"
+									ghost
+									size="lg"
+									color="warning"
+								>
+									Check it out
+								</Button>
+								<StarRating className="hidden sm:block" rating={rating} />
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="right z-10 flex h-full items-start justify-end">
+					<MetacriticSquare
+						className="p-3 text-sm sm:p-4 sm:text-xl"
+						score={parseInt(metacritic)}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default GameCardCenterStage;
+export default GameCardCenterStage
