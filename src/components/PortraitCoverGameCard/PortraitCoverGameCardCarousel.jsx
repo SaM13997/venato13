a02f589c06364fe74210c40935060094252e67dd
Sarthak <androiddeveloper251@gmail.com>
2023-10-19 16:02:58 -0400

    adapted the portraitGameCard to swiper

--- src/components/PortraitCoverGameCard/PortraitCoverGameCardCarousel.jsx
+++ src/components/PortraitCoverGameCard/PortraitCoverGameCardCarousel.jsx
@@ -5,26 +5,35 @@ import Glider from 'react-glider'
 import 'glider-js/glider.min.css'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
 import 'swiper/css'
+import 'swiper/css/navigation'
 
 function PortraitCoverGameCardCarousel({ data, headingText }) {
 	// console.log(Swiper)
 	const games = JSON.parse(data.value)
 	const gamess = [1, 2, 3, 3, 4, 4, 5, 5]
 	return (
-		<Swiper
-			breakpoints={{
-				1348: {
-					slidesPerView: 2,
-				},
-			}}
-		>
-			{games.map((game, index) => (
-				<SwiperSlide key={index}>
-					<PortraitCoverGameCard game={game} />
-				</SwiperSlide>
-			))}
-		</Swiper>
+		<div>
+			<p className="mb-1 mt-4 text-3xl">{headingText}</p>
+			<hr className="mb-1 h-2 w-1/5" />
+			<Swiper
+				modules={[Navigation]}
+				spaceBetween={20}
+				navigation
+				breakpoints={{
+					1348: {
+						slidesPerView: 2,
+					},
+				}}
+			>
+				{games.map((game, index) => (
+					<SwiperSlide key={index}>
+						<PortraitCoverGameCard game={game} />
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
 	)
 	// return (
 	// 	<div className="mt-2 flex max-h-fit w-full flex-col justify-center">
