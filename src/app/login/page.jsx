ca21ca961cd364bd0b0f7d344133c1f26db7a8c8
Sarthak <sarthakmalhotra251@gmail.com>
2023-12-26 23:50:47 +0530

    ifixed form layout

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,6 @@
 export default function Login() {
 	return (
-		<form action="/auth/login" method="post">
+		<form className='flex' action="/auth/login" method="post">
 			<label htmlFor="email">Email</label>
 			<input name="email" />
 			<label htmlFor="password">Password</label>
